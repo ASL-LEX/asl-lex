7b5f0163ac13fca23364860ae188190ab9bfaac3
Ben Tanen <ben.tanen@gmail.com>
2015-02-09 12:02:08 -0500

    added minor data handling

--- js/filter.js
+++ js/filter.js
@@ -1,16 +1,66 @@
+var data = {
+        'ASL Frequency (M)': {
+        	valueA: null,
+		valueB: null 
+        }, 'ASL Frequency (Z)': {
+        	valueA: null,
+		valueB: null
+        }, 'ASL Frequency (SD)': {
+		valueA: null,
+		valueB: null
+        }, 'Sign Type': {
+        	valueA: null,
+		valueB: null
+        }, 'Selected Fingers': {
+		valueA: null,
+		valueB: null
+        }, 'Flexion': {
+        	valueA: null,
+		valueB: null
+        }, 'Major Location': {
+		valueA: null,
+		valueB: null
+        }, 'Minor Location': {
+        	valueA: null,
+		valueB: null
+        }, 'Movement': {
+		valueA: null,
+		valueB: null
+        }, 'Iconicity (M)': {
+        	valueA: null,
+		valueB: null
+        }, 'Iconicity (Z)': {
+		valueA: null,
+		valueB: null
+        }, 'Iconicity (SD)': {
+        	valueA: null,
+		valueB: null
+        }, 'Part of Speech': {
+		valueA: null,
+		valueB: null
+	}	
+};
+
+var optionTitle = '';
 
 $(document).ready(function() {
-    $('.constrain-btn').jBox('Confirm',{
+    popup_A = new jBox('Confirm',{
         attach: $('.constrain-btn'),
-        getTitle: 'data-jbox-title',
-        content: $('#jBox-slider-grab'),
         width: 350,
         height: 100,
-        confirmButton: "Submit"
+        confirmButton: "Submit",
+        onOpen: function() {
+                optionTitle = this.source['0'].dataset['jboxTitle'];
+                $('#constraint-A').val(data[optionTitle].valueA);
+                $('#constraint-B').val(data[optionTitle].valueB);
+        },
+        getTitle: 'data-jbox-title',
+        content: $('#jBox-slider-grab')
     });
 });
 
 function confirm() {
-    console.log($('#range-A').val());
-    console.log($('#range-B').val());
+    // need to validate data
+    data[optionTitle].valueA = $('#constraint-A').val();
+    data[optionTitle].valueB = $('#constraint-B').val();
 }
