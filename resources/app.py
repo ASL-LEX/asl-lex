9100473591987776472a0de00da0d1ba674ab832
FanonX Rogers <fanonxdevelops@gmail.com>
2019-06-20 18:44:35 -0400

    arg parsing: added the ability to pass in a cmd line args that will be feed in as parameters to the pyND module

--- resources/app.py
+++ resources/app.py
@@ -1,21 +1,27 @@
+import config
+import nd.neighbors as nd
+
 import pandas as pd
 import multiprocessing as mp
 import sys
-import os
+import argparse
 
 from itertools import product
 
-# sys.path(0, os.getcwd() + '/resources')
+parser = argparse.ArgumentParser(description='Filter arguments to run in order to create neighbor density values, nodes and edges')
+parser.add_argument("feature_list",type=str, help="A list of strings containing features. This should be the exact same as the columns feed into pyND", nargs='+')
+parser.add_argument("num_pynd",type=int, help="an integer indicating how many features are allowed to differ between the target word and the candidate")
+parser.add_argument("file_name",type=str, help="string that will be the file name of the nodes + edges csv files generated from pyND package")
 
-import config as CONFIG
-import nd.neighbors as nd
+args = parser.parse_args()
 
-""" Script to automate/update Nodes + edges CSV files which will be reflected on the website """
+
+""" Script to automate/update Nodes + edges JSON csv files which will be reflected on the website """
 
 # convert the needed files into dataframes
 # need to use the PyND package to get the onemiss_neighbors + onemiss_nd files
 # sign_df contains all properies of each sign
-sign_df = pd.read_csv(CONFIG.sign_data_file)
+sign_df = pd.read_csv(config.sign_data_file)
 # get rid of the video columns
 sign_df = sign_df.drop(columns=['YouTube Video', 'Vimeo Video', 'ClipLength(ms)'], axis=1)
 
@@ -42,33 +48,45 @@ def create_nd(feature_list, allowed_misses, file_name):
     nodes_df, edges_df = feature_nd._Compute()
 
     # write the nodes and edges to CSVs
-    feature_nd.WriteCSVs(CONFIG.data_path, file_name)
+    feature_nd.WriteCSVs(config.data_path, file_name)
 
     print("Nodes and edges files created! ")
-
+# add major location & minor location
 default_features = [col for col in sign_df.columns[48:63]]
 lexical_features = [col for col in sign_df.columns[4:46]]
 phonological_features = [col for col in sign_df.columns[47:]]
 
 """
-    feature list is a list of tuples
-    This is needed to run parallel processing
-    we pass in this list so the create_nd function
-    will be able to apply these tuple values as arguments
+    feature set is a list of tuples -- needed for the parallel processing
+    tuple[0]: a list of strings
+    tuple[1]: number
+    tuple[2]: a string
+
+    running the script has to be executed as
+    python3 col_name_1 col_name_2 col_name_3..etc num file_name
+
+    example: python3 EntryID LemmaID Code 1 default
 """
-feature_list = [
-    (default_features, 1, 'default'),
-    (lexical_features, 0, 'lex'),
-    (phonological_features, 0, 'phono')
-]
+
+# args that are passed into the command line once the script is run
+features = args.feature_list
+pynd_num = args.num_pynd
+export_file = args.file_name
+
+# have to wrap it in a list of tuples in order to complete parallel processing
+feature_set = [(features, pynd_num, export_file)]
+
 
 def main():
     # creating pool for multiprocessing
     # will run the process in parallel
+
+    # with mp.Pool() as pool:
+    #     results = pool.map(create_nd, feature_set)
+
     with mp.Pool() as pool:
-        results = pool.starmap(create_nd, feature_list)
+        results = pool.starmap(create_nd, feature_set)
+
 
 if __name__ == '__main__':
     main()
-
-
--- resources/config.py
+++ resources/config.py
@@ -1,13 +1,18 @@
 from pathlib import Path
+from dotenv import load_dotenv
 
-old_data_folder = Path("data/")
-new_data_folder = Path("new-data/")
+# paths - will work on all OS
+old_data_folder = Path("old-data/")
+new_data_folder = Path("data/")
+env_path = Path(".") / ".env"
+
+# load in all modules need to run the script
+load_dotenv(dotenv_path=env_path)
 
 # files
 onemiss_nd_file = old_data_folder / 'osfstorage-archive/onemiss-nd.csv'
 onemiss_neighbors_file = old_data_folder / 'osfstorage-archive/onemiss-neighbors.csv'
 phonology_coding_file = old_data_folder / 'osfstorage-archive/PhonologyCoding.csv'
-
 sign_data_file = new_data_folder / 'signdata.csv'
 
 keys_file = old_data_folder / 'osfstorage-archive/Key/Key.csv'
