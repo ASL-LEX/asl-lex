d045290dab2a7c82121aab475bcb43a57b1133e1
FanonX Rogers <fanonxdevelops@gmail.com>
2019-07-09 11:34:11 -0400

    Clean python script

--- scripts/app.py
+++ scripts/app.py
@@ -2,7 +2,6 @@ import pandas as pd
 import config
 import nd.neighbors as nd
 
-import multiprocessing as mp
 import sys
 import argparse
 
@@ -18,20 +17,19 @@ parser.add_argument(dest="export_file", type=str, help="string that will be the 
 
 args = parser.parse_args()
 
-# convert the needed files into dataframes
 # need to use the PyND package to get the onemiss_neighbors + onemiss_nd files
-# sign_df contains all properies of each sign
+"""sign_df contains all properies of each sign"""
 sign_df = pd.read_csv(config.sign_data_file)
 subset_df = pd.read_csv(config.subset_data_file)
+
 # get rid of the video columns
 sign_df = sign_df.drop(columns=['YouTube Video', 'Vimeo Video', 'ClipLength(ms)'], axis=1)
 
-
 def create_nd(data_df, feature_list, allowed_misses, file_name):
     """
-        Helper function to convert into nodes + edges files
-
+        Helper function to convert data nodes + edges files
         Args:
+            data_df: Pandas dataframe containing the data
             feature_list: (List of str) a list of features that edges will be generated from
             allowed_misses: (Int) - look at pynd Neighbors documentation
             file_name: The name of the nodes + edges file that will be generated from the features
@@ -40,7 +38,7 @@ def create_nd(data_df, feature_list, allowed_misses, file_name):
             Creates two csv files in the data folder named file_name-neighbors and file_name-nd
     """
 
-    print(f"Creating nodes & edges csv files for {file_name} features ")
+    print(f"Creating nodes & edges csv files for {file_name} features.")
     print("This will take awhile...")
 
     # create the neighbors object
@@ -57,14 +55,14 @@ def create_nd(data_df, feature_list, allowed_misses, file_name):
 
 """
     feature set is a list of tuples -- needed for the parallel processing
-    tuple[0]: a list of strings
+    tuple[0]: string - which represents a string, .split(",")
     tuple[1]: number
-    tuple[2]: a string
+    tuple[2]: string
 
     running the script has to be executed as
     python3 col_name_1 col_name_2 col_name_3..etc num file_name
 
-    example: python3 EntryID LemmaID Code 1 default
+    example: python3 "EntryID,LemmaID,Code" 1 default
 """
 
 # args that are passed into the command line once the script is run
@@ -72,15 +70,8 @@ features = args.feature_list
 pynd_num = args.num_pynd
 export_file = args.export_file
 
-# have to wrap it in a list of tuples in order to complete parallel processing
-feature_set = [(features, pynd_num, export_file)]
-
 
 def main():
-    # creating pool for multiprocessing
-    # will run the process in parallel
-    # with mp.Pool() as pool:
-    #     results = pool.starmap(create_nd, feature_set)
     create_nd(sign_df, features, pynd_num, export_file)
     # create_nd(subset_df, features, pynd_num, export_file)
 
--- scripts/job.sh
+++ scripts/job.sh
@@ -19,7 +19,7 @@
 #$ -l mem_per_core=8G
 
 # module version to be used
-module load python3/3.7.0
+module load python3/3.7
 
 DEFAULT_FEATURES="SelectedFingers.2.0,Flexion.2.0,FlexionChange.2.0,Spread.2.0,SpreadChange.2.0,ThumbPosition.2.0,ThumbContact.2.0,SignType.2.0,Movement.2.0,MajorLocation.2.0,MinorLocation.2.0,SecondMinorLocation.2.0,Contact.2.0,NonDominantHandshape.2.0,UlnarRotation.2.0"
 LEXICAL_FEATURES="SignFrequency(M),SignFrequency(SD),SignFrequency(Z),SignFrequency(N),PercentUnknown,SignFrequency(M-Native),SignFrequency(SD-Native),SignFreq(Z-native),SignFrequency(N-Native),PercentUnknown(Native),PercentGlossAgreement,PercentGlossAgreement(Native),Iconicity(M),Iconicity(SD),Iconicity(Z),Iconicity(N),Iconicity_ID,SubtLexUSLog10WF,LexicalClass,SignOnset(ms),SignOffset(ms),SignLength(ms),MinimalNeighborhoodDensity,MaximalNeighborhoodDensity,Parameter-BasedNeighborhoodDensity,D.Iconicity(M) all,D.Iconicity(SD) all,D.Iconicity(N) all,D.Iconicity(Z) all,D.Iconicity(M) native,D.Iconicity(SD) native,D.Iconicity(N) native,D.Iconicity(Z) native,Complexity,RightWristX,RightWristcentroidY,DistanceRightWristCentroid2Nose,IconicityType,Initialized.2.0,FingerspelledLoanSign.2.0,Compound.2.0,NumberOfMorphemes.2.0,Handshape.2.0"
@@ -30,7 +30,7 @@ EXPORT_FILE_NAME="default"
 
 # executing script
 # default parameters to create neighborhood density & edges
-python app.py "$DEFAULT_FEATURES" $ALLOWED_MISSES $EXPORT_FILE_NAME
+python app.py "$DEFAULT_FEATURES" $ALLOWED_MISSES "$EXPORT_FILE_NAME"
 
 
 
